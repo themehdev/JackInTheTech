25fe0e8d0cb09cf155c3374745f73558c1723b37
Michael <107226999+themehdev@users.noreply.github.com>
2025-11-18 10:38:34 -0800

    Update AutoWTime.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWTime.java
@@ -53,7 +53,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="CrossLineAuto", group="Robot")
+@Autonomous//(name="CrossLineAuto", group="Robot")
 //@Disabled
 public class AutoWTime extends LinearOpMode {
 
