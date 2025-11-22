b64ebf1be3bc4b501e32774241ce6e10c3306c32
Michael <107226999+themehdev@users.noreply.github.com>
2025-11-22 11:57:07 -0800

    fixed some things with the arm
    they work on tims bot so we should be good.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlacerIO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlacerIO.java
@@ -13,29 +13,35 @@ public interface PlacerIO {
         double armTarget = 0.0;
         double armPow = 0.0;
 
-        double pincherPos = 0.0;
+        boolean armFinished = true;
+
+        double pinchPos = 0.0;
 
         @Override
         public void toLog(@NonNull LogTable logTable) {
             logTable.put("arm/pos", armPos);
             logTable.put("arm/target", armTarget);
             logTable.put("arm/pow", armPow);
+            logTable.put("arm/finished", armFinished);
 
-            logTable.put("pincher/pos", pincherPos);
+            logTable.put("pincher/pos", pinchPos);
         }
 
         public void fromLog(@NonNull LogTable logTable) {
             armPos = logTable.get("arm/pos", armPos);
             armTarget = logTable.get("arm/target", armTarget);
             armPow = logTable.get("arm/pow", armPow);
+            armFinished = logTable.get("arm/finished", armFinished);
 
-            pincherPos = logTable.get("pincher/pos", pincherPos);
+            pinchPos = logTable.get("pincher/pos", pinchPos);
         }
     }
 
     public default void updateInputs(PlacerIOInputs inputs){}
 
     public default void setPincherTargetPos(double pos){}
+
+    public default void setArmPower(double pow){}
 
     public default void setArmTargetPos(double pos){}
 }
